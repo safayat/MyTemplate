0b9cc1195e673eb52d0140300c6831b196a9450e
safayat <safayat@cefalo.com>
2016-10-09 22:20:30 +0600

    db info

---
+++ src/main/resources/db.sql
@@ -0,0 +1,7 @@
+insert into user(username,email,password) values('admin','admin@template.com','$2a$10$b4I0sIIgb4Mkwo3JQQ0m5OULspi8T6wpd/GT2drPV/3T0mI2KKLPC');
+insert into user_role(userId,roleName) values(1,1);
+
+
+
+
+
